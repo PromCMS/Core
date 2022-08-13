1f7611a8aaa1b43b4202e1330da13ba392572a26
Ondřej Langr <hi@ondrejlangr.cz>
2022-08-13 23:07:10 +0200

    Config base should be prop values from child

--- src/Config/ConfigBase.php
+++ src/Config/ConfigBase.php
@@ -4,7 +4,7 @@ namespace PromCMS\Core\Config;
 class ConfigBase {
   function __construct(array $config)
   {
-    $keys = array_keys(get_object_vars($this));
+    $keys = array_keys(get_class_vars(static::class));
 
     foreach ($keys as $key) {
       if (isset($config[$key])) {
