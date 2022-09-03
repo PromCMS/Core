002688a8aa51292233163631d3f6d2b1d1b3a34a
Ondřej Langr <hi@ondrejlangr.cz>
2022-09-03 16:45:04 +0200

    Remove locales filesystem and rename entry in inject container from file-cache-filesystem to cache-filesystem

--- src/Bootstrap/FlySystem.php
+++ src/Bootstrap/FlySystem.php
@@ -18,18 +18,12 @@ class FlySystem implements AppModuleInterface
     $adapter = new LocalFilesystemAdapter($fsConfig->uploadsPath);
     $filesystem = new Filesystem($adapter);
 
-    // Locales path
-    $localesAdapter = new LocalFilesystemAdapter($fsConfig->localesPath);
-    $localesFilesystem = new Filesystem($localesAdapter);
-
     // Cache path
     $fileCacheAdapter = new LocalFilesystemAdapter($fsConfig->cachePath);
     $fileCacheFilesystem = new Filesystem($fileCacheAdapter);
 
     // TODO: Do not create this much fs instance - we need to create one and think about different solution
-    // Set locales filesystem module and attach it to container
     $container->set('filesystem', $filesystem);
-    $container->set('locales-filesystem', $localesFilesystem);
-    $container->set('file-cache-filesystem', $fileCacheFilesystem);
+    $container->set('cache-filesystem', $fileCacheFilesystem);
   }
 }
