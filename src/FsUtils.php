81c32b00acf35b7e3933b6ddb0eaf0465ab3469a
Ondřej Langr <hi@ondrejlangr.cz>
2023-09-02 15:34:37 +0200

    Create FsUtils with recursive remove

---
+++ src/FsUtils.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace PromCMS\Core;
+
+class FsUtils {
+  /**
+   * Recursively deletes directory and its contents
+   */
+  static function rrmdir($dir) { 
+    if (is_dir($dir)) { 
+      $objects = scandir($dir);
+      foreach ($objects as $object) { 
+        if ($object != "." && $object != "..") { 
+          if (is_dir($dir. DIRECTORY_SEPARATOR .$object) && !is_link($dir."/".$object))
+            static::rrmdir($dir. DIRECTORY_SEPARATOR .$object);
+          else
+            unlink($dir. DIRECTORY_SEPARATOR .$object); 
+        } 
+      }
+      rmdir($dir); 
+    } 
+  }
+}
