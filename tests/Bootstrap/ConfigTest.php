fe9e21d0c8a962e6098507df772d00c5ca12174c
Ondřej Langr <hi@ondrejlangr.cz>
2023-10-05 12:04:58 +0200

    breaking: Unify .env variable for languages into one LANGUAGES.
    First language in list is taken as default one.

---
+++ tests/Bootstrap/ConfigTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace PromCMS\Tests\Bootstrap;
+
+use DI\Container;
+use PromCMS\Tests\AppTestCase;
+use PromCMS\Core\Bootstrap\Config as ConfigBootstrap;
+use PromCMS\Core\Config as AppConfig;
+
+final class ConfigTest extends AppTestCase
+{
+    static string $testProjectRoot;
+
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+    }
+
+    public function test_toArray_converts_recursive_correctly()
+    {
+        $container = new Container();
+        $container->set('app.root', static::$testProjectRoot);
+        (new ConfigBootstrap())->run(null, $container);
+
+        $this->assertEqualsCanonicalizing($container->get(AppConfig::class)->__toArray(), [
+            'app' => [
+                'name' => 'PromCMS Test Project',
+                'root' => static::$testProjectRoot,
+                'url' => 'http://localhost:3004',
+                'prefix' => '',
+                'baseUrl' => 'http://localhost:3004',
+            ],
+            'security' => [
+                'session' => [
+                    'lifetime' => 3600,
+                ],
+                'token' => [
+                    'lifetime' => 86400,
+                ],
+            ],
+            'db' => [
+                'root' => static::$testProjectRoot . '/.database',
+                'storeConfig' => [
+                    'auto_cache' => false,
+                    'cache_lifetime' => null,
+                    'timeout' => false,
+                    'primary_key' => 'id',
+                    'search' => [
+                        'min_length' => 2,
+                        'mode' => 'or',
+                        'score_key' => 'scoreKey',
+                        'algorithm' => 1,
+                    ],
+                ],
+            ],
+            'env' => [
+                'development' => true,
+                'debug' => true,
+                'env' => 'development',
+            ],
+            'fs' => [
+                'cachePath' => static::$testProjectRoot . '/cache/files',
+                'localesPath' => static::$testProjectRoot . '/locales',
+                'uploadsPath' => static::$testProjectRoot . '/uploads',
+            ],
+            'i18n' => [
+                'default' => 'en',
+                'languages' => ['en'],
+            ],
+            'system' => [
+                'modules' => [
+                    'modelsFolderName' => 'Models',
+                    'controllersFolderName' => 'Controllers',
+                ],
+            ],
+        ]);
+    }
+}
