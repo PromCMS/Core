d1c63232a1093cc24c7b3e5cc5e4e1de9bc902d7
Ondřej Langr <hi@ondrejlangr.cz>
2023-10-05 12:03:18 +0200

    feature: Add __toArray which converts config to array recursively.

--- src/Config/ConfigBase.php
+++ src/Config/ConfigBase.php
@@ -12,4 +12,19 @@ class ConfigBase {
       }
     }
   }
+
+  /**
+   * Converts properties to array recursively
+   */
+  function __toArray(): array {
+    $properties = get_object_vars($this);
+
+    foreach ($properties as $propertyName => $propertyValue) {
+      if ($propertyValue instanceof ConfigBase) {
+        $properties[$propertyName] = $propertyValue->__toArray();
+      }
+    }
+
+    return $properties;
+  }
 }
