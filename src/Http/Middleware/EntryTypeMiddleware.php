203f90b10840c8911aff526acf48890953445018
Ondřej Langr <hi@ondrejlangr.cz>
2023-10-08 00:50:59 +0200

    feature: Disable disabled models from api routes.

--- src/Http/Middleware/EntryTypeMiddleware.php
+++ src/Http/Middleware/EntryTypeMiddleware.php
@@ -10,92 +10,81 @@ use Slim\Routing\RouteContext;
 
 class EntryTypeMiddleware
 {
-  private $loadedModels;
-  private bool $singletonsOnly;
+    private $loadedModels;
+    private bool $singletonsOnly;
 
-  public function __construct($container, $singletonsOnly = true)
-  {
-    $this->loadedModels = $container->get('sysinfo')['loadedModels'];
-    $this->singletonsOnly = $singletonsOnly;
-  }
-
-  /**
-   * Checks if model is loaded/exists and returns the real, usable model name
-   */
-  private function getModelFromArg(string $modelName)
-  {
-    $modelIndex = array_search(
-      strtolower($modelName),
-      // filter out those files we wanted to opt out
-      array_filter(
-        // format all model names to be all in lowercase
-        array_map(function ($modelName) {
-          $slicedModelName = explode("\\", $modelName);
-
-          return strtolower(end($slicedModelName));
-        }, $this->loadedModels),
-        function ($modelName) {
-          return !in_array($modelName, [
-            'files',
-            'users',
-            'userroles',
-            'generaltranslations',
-          ]);
-        },
-      ),
-    );
-    if ($modelIndex === false) {
-      return false;
+    public function __construct($container, $singletonsOnly = true)
+    {
+        $this->loadedModels = $container->get('sysinfo')['loadedModels'];
+        $this->singletonsOnly = $singletonsOnly;
     }
 
-    $modelName = $this->loadedModels[$modelIndex];
+    /**
+     * Checks if model is loaded/exists and returns the real, usable model name
+     */
+    private function getModelFromArg(string $modelName)
+    {
+        $modelIndex = array_search(
+            strtolower($modelName),
+            // filter out those files we wanted to opt out
+            array_filter(
+                // format all model names to be all in lowercase
+                array_map(function ($modelName) {
+                    $slicedModelName = explode('\\', $modelName);
 
-    return "\\$modelName";
-  }
+                    return strtolower(end($slicedModelName));
+                }, $this->loadedModels),
+                function ($modelName) {
+                    return !in_array($modelName, ['files', 'users', 'userroles', 'generaltranslations']);
+                },
+            ),
+        );
+        if ($modelIndex === false) {
+            return false;
+        }
 
-  /**
-   * Permission middleware class, it interacts with session and gets if in session theres a sufficient user role for provided route
-   *
-   * @param  \Psr\Http\Message\ServerRequestInterface $request  PSR7 request
-   * @param  \Psr\Http\Message\ResponseInterface      $response PSR7 response
-   * @param  callable                                 $next     Next middleware
-   *
-   * @return \Psr\Http\Message\ResponseInterface
-   */
-  public function __invoke(Request $request, RequestHandler $handler): Response
-  {
-    $routeContext = RouteContext::fromRequest($request);
-    $route = $routeContext->getRoute();
-    $modelInstancePath = $this->getModelFromArg($route->getArgument('modelId'));
+        $modelName = $this->loadedModels[$modelIndex];
 
-    if ($modelInstancePath === false) {
-      $response = new Response();
-
-      return $response
-        ->withStatus(404)
-        ->withHeader('Content-Description', 'model does not exist');
+        return "\\$modelName";
     }
 
-    $modelInstance = new $modelInstancePath();
+    /**
+     * Permission middleware class, it interacts with session and gets if in session theres a sufficient user role for provided route
+     *
+     * @param  \Psr\Http\Message\ServerRequestInterface $request  PSR7 request
+     * @param  \Psr\Http\Message\ResponseInterface      $response PSR7 response
+     * @param  callable                                 $next     Next middleware
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function __invoke(Request $request, RequestHandler $handler): Response
+    {
+        $routeContext = RouteContext::fromRequest($request);
+        $route = $routeContext->getRoute();
+        $modelInstancePath = $this->getModelFromArg($route->getArgument('modelId'));
 
-    if (($this->singletonsOnly && ($modelInstance instanceof SingletonModel) == FALSE) || !$this->singletonsOnly && $modelInstance instanceof SingletonModel) {
-      $response = new Response();
+        if ($modelInstancePath === false) {
+            $response = new Response();
 
-      return $response
-        ->withStatus(404)
-        ->withHeader('Content-Description', 'model does not exist');
+            return $response->withStatus(404)->withHeader('Content-Description', 'model does not exist');
+        }
+
+        $modelInstance = new $modelInstancePath();
+
+        if (
+          ($this->singletonsOnly && $modelInstance instanceof SingletonModel == false) || 
+          (!$this->singletonsOnly && $modelInstance instanceof SingletonModel) || 
+          (isset($modelInstance->getSummary()->enabled) && !$modelInstance->getSummary()->enabled)
+        ) {
+            $response = new Response();
+
+            return $response->withStatus(404)->withHeader('Content-Description', 'model does not exist or is not enabled for use through api');
+        }
+
+        // Attach on request to pass the model instance info
+        $request = $request->withAttribute('model-instance', $modelInstance);
+        $request = $request->withAttribute('model-instance-path', $modelInstancePath);
+
+        return $handler->handle($request);
     }
-
-    // Attach on request to pass the model instance info
-    $request = $request->withAttribute(
-      'model-instance',
-      $modelInstance,
-    );
-    $request = $request->withAttribute(
-      'model-instance-path',
-      $modelInstancePath,
-    );
-
-    return $handler->handle($request);
-  }
 }
