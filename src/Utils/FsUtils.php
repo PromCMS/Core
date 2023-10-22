9664284101ea009af3070a9cc277d89ced4889b8
Ondřej Langr <hi@ondrejlangr.cz>
2023-10-22 12:23:58 +0200

    fix: Do not load folder in autoload controllers and add todo for the removal of autoloadModels.

--- src/Utils/FsUtils.php
+++ src/Utils/FsUtils.php
@@ -1,26 +1,29 @@
 <?php
 
 namespace PromCMS\Core\Utils;
+
 use PromCMS\Core\Module;
 use PromCMS\Core\Path;
 
-class FsUtils {
+class FsUtils
+{
   /**
    * Recursively deletes directory and its contents
    */
-  static function rrmdir($dir) { 
-    if (is_dir($dir)) { 
+  static function rrmdir($dir)
+  {
+    if (is_dir($dir)) {
       $objects = scandir($dir);
-      foreach ($objects as $object) { 
-        if ($object != "." && $object != "..") { 
-          if (is_dir($dir. DIRECTORY_SEPARATOR .$object) && !is_link($dir."/".$object))
-            static::rrmdir($dir. DIRECTORY_SEPARATOR .$object);
+      foreach ($objects as $object) {
+        if ($object != "." && $object != "..") {
+          if (is_dir($dir . DIRECTORY_SEPARATOR . $object) && !is_link($dir . "/" . $object))
+            static::rrmdir($dir . DIRECTORY_SEPARATOR . $object);
           else
-            unlink($dir. DIRECTORY_SEPARATOR .$object); 
-        } 
+            unlink($dir . DIRECTORY_SEPARATOR . $object);
+        }
       }
-      rmdir($dir); 
-    } 
+      rmdir($dir);
+    }
   }
 
   /**
@@ -30,22 +33,23 @@ class FsUtils {
    *                             This can be relative or absolute file path to filesystem, or location on the internet. 
    *                             This can also be a path with @modules:<module name> prefix to resolve into module right away
    */
-  static function readFile(string $fileLocation) {
+  static function readFile(string $fileLocation)
+  {
     $path = $fileLocation;
 
     if (str_starts_with($fileLocation, '@modules:')) {
       $chunks = explode('/', $fileLocation);
 
       $path = Path::join(
-        (new Module($chunks[0]))->getPath(), 
+        (new Module($chunks[0]))->getPath(),
         implode('/', array_slice($chunks, 1))
       );
-    } 
+    }
 
     return file_get_contents($path);
   }
 
-    /**
+  /**
    * Get content of defined directory
    */
   public static function getDirContents($dir, &$results = [])
@@ -66,6 +70,7 @@ class FsUtils {
   }
 
 
+  // TODO - deprecate this and use psr autoload instead
   /**
    * Imports all php scripts for specified folder
    * @return string[]|boolean Returns an array of imported paths
@@ -79,6 +84,11 @@ class FsUtils {
 
     $filePaths = static::getDirContents($pathToFolder);
     foreach ($filePaths as $filePath) {
+      // do not load dir, this is just one level only
+      if (is_dir($filePath)) {
+        continue;
+      }
+
       include_once $filePath;
       $importedFilePaths[] = $filePath;
     }
@@ -86,7 +96,7 @@ class FsUtils {
     return $importedFilePaths;
   }
 
-    // TODO: make a better solution to this class search
+  // TODO: make a better solution to this class search
   /**
    * Auto-loads models for specified module root. This is primarily used by modules.
    * @return string[]|false An array of module names or
