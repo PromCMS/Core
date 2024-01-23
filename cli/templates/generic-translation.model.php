b132e1088f9f18756c7e917ba98119b3e25750df
Ondřej Langr <hi@ondrejlangr.cz>
2024-01-23 18:44:42 +0100

    Fixes indexes of created models

--- cli/templates/generic-translation.model.php
+++ cli/templates/generic-translation.model.php
@@ -10,6 +10,6 @@ namespace <?php echo $entity->namespace; ?>;
 
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity, ORM\Table(name: '<?php echo $entity->getTranslationTableName() ?>'), ORM\UniqueConstraint(name: 'lookup_unique_idx', columns: ['locale', 'object_id', 'field']), ORM\HasLifecycleCallbacks]
+#[ORM\Entity, ORM\Table(name: '<?php echo $entity->getTranslationTableName() ?>'), ORM\UniqueConstraint(name: '<?php echo $entity->getTranslationTableName() ?>_unique_idx', columns: ['locale', 'object_id', 'field']), ORM\HasLifecycleCallbacks]
 class <?php echo $entityName ?> extends Base\<?php echo $entityName ?> {
 }
--- src/Database/Models/FileTranslation.php
+++ src/Database/Models/FileTranslation.php
@@ -4,6 +4,6 @@ namespace PromCMS\Core\Database\Models;
 
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity, ORM\Table(name: 'prom__files_translations'), ORM\UniqueConstraint(name: 'lookup_unique_idx', columns: ['locale', 'object_id', 'field']), ORM\HasLifecycleCallbacks]
+#[ORM\Entity, ORM\Table(name: 'prom__files_translations'), ORM\UniqueConstraint(name: 'prom__files_translations_unique_idx', columns: ['locale', 'object_id', 'field']), ORM\HasLifecycleCallbacks]
 class FileTranslation extends Base\FileTranslation {
 }
