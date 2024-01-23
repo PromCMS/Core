fa8d040682c103bf8bd3151987da83f4dab89a78
Ondřej Langr <hi@ondrejlangr.cz>
2024-01-23 15:36:42 +0100

    Update localization middleware as something that could accept language from multiple places

--- src/Internal/Bootstrap/Middlewares.php
+++ src/Internal/Bootstrap/Middlewares.php
@@ -2,6 +2,7 @@
 
 namespace PromCMS\Core\Internal\Bootstrap;
 
+use DI\Container;
 use PromCMS\Core\Services\LocalizationService;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
@@ -11,10 +12,12 @@ use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
  */
 class Middlewares implements AppModuleInterface
 {
-  public function run($app, $container)
+  public function run($app, Container $container)
   {
     $localizationMiddleware = function (Request $request, RequestHandler $handler) use ($container) {
-      $path = $request->getUri()->getPath();
+      $requestUri = $request->getUri();
+      $path = $requestUri->getPath();
+      $requestQueryParams = $request->getQueryParams();
       $localizationService = $container->get(LocalizationService::class);
       $possibleLanguages = $localizationService->getSupportedLanguages();
       $possibleLanguagesAsJoinedString = implode('|', $possibleLanguages);
@@ -23,8 +26,17 @@ class Middlewares implements AppModuleInterface
         $firstSegmentAsLanguage = explode('/', $path)[1];
 
         $localizationService->setCurrentLanguage($firstSegmentAsLanguage);
+      } else if (!empty($acceptLanguages = $request->getHeader('accept-language'))) {
+        $languageFromRequest = $acceptLanguages[0];
+
+        $localizationService->setCurrentLanguage($languageFromRequest, false);
+      } else if (!empty($requestQueryParams["lang"])) {
+        $languageFromQuery = $requestQueryParams["lang"];
+
+        $localizationService->setCurrentLanguage($languageFromQuery, false);
       }
 
+      $request->withAttribute('lang', $localizationService->getCurrentLanguage());
       return $handler->handle($request);
     };
 
