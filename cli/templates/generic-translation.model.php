8fb93a2baf907a0fa87534aa524c053e7c9142fb
Ondřej Langr <hi@ondrejlangr.cz>
2024-01-24 15:36:25 +0100

    Fixes generict translation template which did not have repository class set

--- cli/templates/generic-translation.model.php
+++ cli/templates/generic-translation.model.php
@@ -4,12 +4,24 @@
  */
 echo "<?php\n";
 $entityName = $entity->getTranslationPhpName();
+
+$attributesAsArray = [
+  "ORM\Entity(repositoryClass: TranslationRepository::class))",
+  "ORM\Table(name: '" . $entity->getTranslationTableName() . "')",
+  "ORM\UniqueConstraint(name: '" . $entity->getTranslationTableName() . "_unique_idx', columns: ['locale', 'object_id', 'field'])",
+  "ORM\HasLifecycleCallbacks"
+];
+
+if ($entity->localized) {
+  $attributesAsArray[] = "GedmoMapping\TranslationEntity(class: \\" . $entity->getTranslationClassName() . "::class)";
+}
 ?>
 
 namespace <?php echo $entity->namespace; ?>;
 
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Translatable\Entity\Repository\TranslationRepository;
 
-#[ORM\Entity, ORM\Table(name: '<?php echo $entity->getTranslationTableName() ?>'), ORM\UniqueConstraint(name: '<?php echo $entity->getTranslationTableName() ?>_unique_idx', columns: ['locale', 'object_id', 'field']), ORM\HasLifecycleCallbacks]
+#[<?php echo implode(', ', $attributesAsArray) ?>]
 class <?php echo $entityName ?> extends Base\<?php echo $entityName ?> {
 }
