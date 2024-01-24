df8ee256ca1af39169fc6fcdd9614bd63a5a9973
Ondřej Langr <hi@ondrejlangr.cz>
2024-01-24 12:29:18 +0100

    Fixes middleware for language

--- src/Internal/Bootstrap/Middlewares.php
+++ src/Internal/Bootstrap/Middlewares.php
@@ -22,18 +22,32 @@ class Middlewares implements AppModuleInterface
       $possibleLanguages = $localizationService->getSupportedLanguages();
       $possibleLanguagesAsJoinedString = implode('|', $possibleLanguages);
 
-      if (preg_match("/^\/($possibleLanguagesAsJoinedString)($|\/\.*)/", $path)) {
-        $firstSegmentAsLanguage = explode('/', $path)[1];
-
-        $localizationService->setCurrentLanguage($firstSegmentAsLanguage);
-      } else if (!empty($acceptLanguages = $request->getHeader('accept-language'))) {
-        $languageFromRequest = $acceptLanguages[0];
-
-        $localizationService->setCurrentLanguage($languageFromRequest, false);
-      } else if (!empty($requestQueryParams["lang"])) {
+      // First priority if 'lang' query parameter
+      if (!empty($requestQueryParams["lang"])) {
         $languageFromQuery = $requestQueryParams["lang"];
 
         $localizationService->setCurrentLanguage($languageFromQuery, false);
+      }
+      // If not defined in query param then check for url
+      else if (preg_match("/^\/($possibleLanguagesAsJoinedString)($|\/\.*)/", $path)) {
+        $firstSegmentAsLanguage = explode('/', $path)[1];
+
+        $localizationService->setCurrentLanguage($firstSegmentAsLanguage);
+      }
+      // fallback to header value
+      else if (!empty($acceptLanguages = $request->getHeader('accept-language'))) {
+        $prefLocalesFromHeader = array_reduce(
+          explode(',', $acceptLanguages[0]),
+          function ($res, $el) {
+            list($l, $q) = array_merge(explode(';q=', $el), [1]);
+            $res[explode('-', $l)[0]] = (float) $q;
+            return $res;
+          },
+          []
+        );
+        arsort($prefLocalesFromHeader);
+
+        $localizationService->setCurrentLanguage(array_keys($prefLocalesFromHeader)[0], false);
       }
 
       return $handler->handle($request->withAttribute('lang', $localizationService->getCurrentLanguage()));
