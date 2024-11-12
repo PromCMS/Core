ce52b03474d20eb05f24a8cb51cf79f9f837c4a1
Ondřej Langr <hi@ondrejlangr.cz>
2024-11-12 14:47:10 +0100

    fix: Fixes missing urldecode to auth info for mailer

--- src/Mailer.php
+++ src/Mailer.php
@@ -32,29 +32,29 @@ class Mailer extends PHPMailer
 
   /**
    * Sets up Mailer with specified config and enables email sending
-   * 
+   *
    * @param Uri $uri An URI instance. Example: new Uri('smtp://user:some-password@some-server:587');
    */
-  public function setup(Uri $uri, string|null $sendFrom = null, string|null $sendFromName = null)
-  {
+  public function setup(
+    Uri $uri,
+    string|null $sendFrom = null,
+    string|null $sendFromName = null
+  ) {
     $this->Host = $uri->getHost();
 
-    if (!empty($mailerPort = $uri->getPort())) {
+    if (!empty(($mailerPort = $uri->getPort()))) {
       $this->Port = $mailerPort;
     }
 
-    if (!empty($userInfo = $uri->getUserInfo())) {
-      [$username, $password] = explode(':', $userInfo);
+    if (!empty(($userInfo = $uri->getUserInfo()))) {
+      [$username, $password] = explode(':', urldecode($userInfo));
 
       $this->Username = $username;
       $this->Password = $password ?? '';
     }
 
-    if (!empty($fromEmailAddress = $sendFrom ?? $this->Username)) {
-      $this->setFrom(
-        $fromEmailAddress,
-        $sendFromName ?? 'PROM Mailer',
-      );
+    if (!empty(($fromEmailAddress = $sendFrom ?? $this->Username))) {
+      $this->setFrom($fromEmailAddress, $sendFromName ?? 'PROM Mailer');
     }
 
     $this->isSetuped = true;
@@ -63,7 +63,8 @@ class Mailer extends PHPMailer
   public function send(): bool
   {
     if (!$this->isSetuped) {
-      $this->ErrorInfo = 'Mailer is not setuped - setup mailer with setup() method on mailer class';
+      $this->ErrorInfo =
+        'Mailer is not setuped - setup mailer with setup() method on mailer class';
       return false;
     }
 
